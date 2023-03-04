06f9f7cc372854c6eb2f9f60e574715f20c4efe2
MrGuigu22 <santiagomier22@gmail.com>
2023-03-04 14:17:26 -0500

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/pruebadeescritorio/PruebaDeEscritorio.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebadeescritorio;
+
+/**
+ *
+ * @author santi
+ */
+public class PruebaDeEscritorio {
+
+    public static void main(String[] args) {
+        int horasTrabajadas = 48;
+        int valorHora = 5000;
+        double porcentajeRetencion = 0.125;
+        int salarioBruto = horasTrabajadas * valorHora;
+        double retencionFuente = salarioBruto * porcentajeRetencion;
+        double salarioNeto = salarioBruto - retencionFuente;
+        System.out.println("el salario bruto del trabajador es: " + salarioBruto);
+        System.out.println("el salario neto del trabajador es: " + salarioNeto);
+        System.out.println("la retencion en la fuente del trabajador es: "+retencionFuente);
+    }
+}
